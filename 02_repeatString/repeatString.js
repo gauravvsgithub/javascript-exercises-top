2389defcdf5cec44cfd007e8481f8e58a3afdf0d
Gaurav Sonawane <54109748+gauravvsgithub@users.noreply.github.com>
2023-01-05 13:33:53 +0530

    return string repeated given times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringbit, noOfTimes) {
+    if(noOfTimes < 0) return "ERROR";
+    let res = ''
+    for(let i=0;i<noOfTimes;i++) {
+        res += stringbit;
+    }
+    return res;
 };
 
 // Do not edit below this line
