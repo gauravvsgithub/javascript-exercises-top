2b3edffc293230d3deb1218ce8944c51ddd8a282
Gaurav Sonawane <54109748+gauravvsgithub@users.noreply.github.com>
2023-01-05 18:03:14 +0530

    reverse string by splitting into array and then reversing and joining back into string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function (string) {
-    let res = '';
-    for(let i=string.length-1;i>=0;i--) {
-        res += string[i];
-    }
-    return res;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
