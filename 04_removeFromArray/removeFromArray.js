cc27f0af4922ba86b1b6f2a7b33b7768d7d943e9
Gaurav Sonawane <54109748+gauravvsgithub@users.noreply.github.com>
2023-01-09 17:03:18 +0530

    remove elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...keys) {
+    for(const key of keys) {
+        while(arr.indexOf(key) > -1) {
+        arr.splice(arr.indexOf(key), 1);
+    }
+    }
+    return arr;
+    
 };
 
 // Do not edit below this line
