09b4b3e9411e3bb779108f643c1dea3198691c55
Gaurav Sonawane <54109748+gauravvsgithub@users.noreply.github.com>
2023-01-10 17:34:19 +0530

    handle edge cases and return sum of numbers between two integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start <= -1 || end <= -1) return "ERROR";
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    let sum = 0;
+    if(start > end) {
+        end = start + end;
+        start = end-start;
+        end = end - start;
+    }
+    let i = start;
+    while(i<=end) {
+        sum+=i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
